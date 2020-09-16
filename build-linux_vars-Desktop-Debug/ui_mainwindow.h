04d8fefefde67587c38d6af7538a2607a13bfe0c
Ваше Имя <you@example.com>
2020-09-16 16:00:46 +0300

    1

--- build-linux_vars-Desktop-Debug/ui_mainwindow.h
+++ build-linux_vars-Desktop-Debug/ui_mainwindow.h
@@ -36,7 +36,7 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(272, 174);
+        MainWindow->resize(510, 267);
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
         tableWidget = new QTableWidget(centralWidget);
@@ -44,8 +44,8 @@ public:
             tableWidget->setColumnCount(1);
         QTableWidgetItem *__qtablewidgetitem = new QTableWidgetItem();
         tableWidget->setHorizontalHeaderItem(0, __qtablewidgetitem);
-        if (tableWidget->rowCount() < 3)
-            tableWidget->setRowCount(3);
+        if (tableWidget->rowCount() < 6)
+            tableWidget->setRowCount(6);
         QTableWidgetItem *__qtablewidgetitem1 = new QTableWidgetItem();
         tableWidget->setVerticalHeaderItem(0, __qtablewidgetitem1);
         QTableWidgetItem *__qtablewidgetitem2 = new QTableWidgetItem();
@@ -53,17 +53,32 @@ public:
         QTableWidgetItem *__qtablewidgetitem3 = new QTableWidgetItem();
         tableWidget->setVerticalHeaderItem(2, __qtablewidgetitem3);
         QTableWidgetItem *__qtablewidgetitem4 = new QTableWidgetItem();
-        tableWidget->setItem(0, 0, __qtablewidgetitem4);
+        tableWidget->setVerticalHeaderItem(3, __qtablewidgetitem4);
         QTableWidgetItem *__qtablewidgetitem5 = new QTableWidgetItem();
-        tableWidget->setItem(1, 0, __qtablewidgetitem5);
+        tableWidget->setVerticalHeaderItem(4, __qtablewidgetitem5);
         QTableWidgetItem *__qtablewidgetitem6 = new QTableWidgetItem();
-        tableWidget->setItem(2, 0, __qtablewidgetitem6);
+        tableWidget->setVerticalHeaderItem(5, __qtablewidgetitem6);
+        QTableWidgetItem *__qtablewidgetitem7 = new QTableWidgetItem();
+        tableWidget->setItem(0, 0, __qtablewidgetitem7);
+        QTableWidgetItem *__qtablewidgetitem8 = new QTableWidgetItem();
+        tableWidget->setItem(1, 0, __qtablewidgetitem8);
+        QTableWidgetItem *__qtablewidgetitem9 = new QTableWidgetItem();
+        tableWidget->setItem(2, 0, __qtablewidgetitem9);
+        QTableWidgetItem *__qtablewidgetitem10 = new QTableWidgetItem();
+        tableWidget->setItem(3, 0, __qtablewidgetitem10);
+        QTableWidgetItem *__qtablewidgetitem11 = new QTableWidgetItem();
+        tableWidget->setItem(4, 0, __qtablewidgetitem11);
+        QTableWidgetItem *__qtablewidgetitem12 = new QTableWidgetItem();
+        tableWidget->setItem(5, 0, __qtablewidgetitem12);
         tableWidget->setObjectName(QStringLiteral("tableWidget"));
-        tableWidget->setGeometry(QRect(0, 0, 391, 151));
+        tableWidget->setGeometry(QRect(0, 0, 511, 221));
+        tableWidget->setMinimumSize(QSize(471, 0));
+        tableWidget->setMaximumSize(QSize(1000, 1000));
+        tableWidget->horizontalHeader()->setDefaultSectionSize(400);
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 272, 22));
+        menuBar->setGeometry(QRect(0, 0, 510, 22));
         MainWindow->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindow);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
@@ -81,20 +96,33 @@ public:
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "\320\241\320\276\321\201\321\202\320\276\321\217\320\275\320\270\320\265 \321\201\320\270\321\201\321\202\320\265\320\274\321\213", Q_NULLPTR));
         QTableWidgetItem *___qtablewidgetitem = tableWidget->verticalHeaderItem(0);
-        ___qtablewidgetitem->setText(QApplication::translate("MainWindow", "\320\227\320\260\320\263\321\200\321\203\320\267\320\272\320\260 \320\277\321\200\320\276\321\206\320\265\321\201\321\201\320\276\321\200\320\260, %", Q_NULLPTR));
+        ___qtablewidgetitem->setText(QApplication::translate("MainWindow", "\320\234\320\276\320\264\320\265\320\273\321\214 \320\277\321\200\320\276\321\206\320\265\321\201\321\201\320\276\321\200\320\260", Q_NULLPTR));
         QTableWidgetItem *___qtablewidgetitem1 = tableWidget->verticalHeaderItem(1);
-        ___qtablewidgetitem1->setText(QApplication::translate("MainWindow", "\320\230\321\201\320\277\320\276\320\273\321\214\320\267\320\276\320\262\320\260\320\275\320\270\320\265 \320\277\320\260\320\274\321\217\321\202\320\270, %", Q_NULLPTR));
+        ___qtablewidgetitem1->setText(QApplication::translate("MainWindow", "\320\247\320\260\321\201\321\202\320\276\321\202\320\260 \320\277\321\200\320\276\321\206\320\265\321\201\321\201\320\276\321\200\320\260", Q_NULLPTR));
         QTableWidgetItem *___qtablewidgetitem2 = tableWidget->verticalHeaderItem(2);
-        ___qtablewidgetitem2->setText(QApplication::translate("MainWindow", "\320\227\320\260\321\200\321\217\320\264 \320\261\320\260\321\202\320\260\321\200\320\265\320\270, %", Q_NULLPTR));
+        ___qtablewidgetitem2->setText(QApplication::translate("MainWindow", "\320\240\320\260\320\267\320\274\320\265\321\200 \320\272\320\265\321\210\320\260 \320\277\321\200\320\276\321\206\320\265\321\201\321\201\320\276\321\200\320\260", Q_NULLPTR));
+        QTableWidgetItem *___qtablewidgetitem3 = tableWidget->verticalHeaderItem(3);
+        ___qtablewidgetitem3->setText(QApplication::translate("MainWindow", "\320\227\320\260\320\263\321\200\321\203\320\267\320\272\320\260 \320\277\321\200\320\276\321\206\320\265\321\201\321\201\320\276\321\200\320\260, %", Q_NULLPTR));
+        QTableWidgetItem *___qtablewidgetitem4 = tableWidget->verticalHeaderItem(4);
+        ___qtablewidgetitem4->setText(QApplication::translate("MainWindow", "\320\230\321\201\320\277\320\276\320\273\321\214\320\267\320\276\320\262\320\260\320\275\320\270\320\265 \320\277\320\260\320\274\321\217\321\202\320\270, %", Q_NULLPTR));
+        QTableWidgetItem *___qtablewidgetitem5 = tableWidget->verticalHeaderItem(5);
+        ___qtablewidgetitem5->setText(QApplication::translate("MainWindow", "\320\227\320\260\321\200\321\217\320\264 \320\261\320\260\321\202\320\260\321\200\320\265\320\270, %", Q_NULLPTR));
 
         const bool __sortingEnabled = tableWidget->isSortingEnabled();
         tableWidget->setSortingEnabled(false);
-        QTableWidgetItem *___qtablewidgetitem3 = tableWidget->item(0, 0);
-        ___qtablewidgetitem3->setText(QApplication::translate("MainWindow", "20", Q_NULLPTR));
-        QTableWidgetItem *___qtablewidgetitem4 = tableWidget->item(1, 0);
-        ___qtablewidgetitem4->setText(QApplication::translate("MainWindow", "30", Q_NULLPTR));
-        QTableWidgetItem *___qtablewidgetitem5 = tableWidget->item(2, 0);
-        ___qtablewidgetitem5->setText(QApplication::translate("MainWindow", "40", Q_NULLPTR));
+        QTableWidgetItem *___qtablewidgetitem6 = tableWidget->item(0, 0);
+        ___qtablewidgetitem6->setText(QApplication::translate("MainWindow", "Intel(R) Xeon(R) CPU E5-2660 0 @ 2.20GHz", Q_NULLPTR));
+        QTableWidgetItem *___qtablewidgetitem7 = tableWidget->item(1, 0);
+        ___qtablewidgetitem7->setText(QApplication::translate("MainWindow", "600.000\n"
+"", Q_NULLPTR));
+        QTableWidgetItem *___qtablewidgetitem8 = tableWidget->item(2, 0);
+        ___qtablewidgetitem8->setText(QApplication::translate("MainWindow", "20480 KB", Q_NULLPTR));
+        QTableWidgetItem *___qtablewidgetitem9 = tableWidget->item(3, 0);
+        ___qtablewidgetitem9->setText(QApplication::translate("MainWindow", "20", Q_NULLPTR));
+        QTableWidgetItem *___qtablewidgetitem10 = tableWidget->item(4, 0);
+        ___qtablewidgetitem10->setText(QApplication::translate("MainWindow", "30", Q_NULLPTR));
+        QTableWidgetItem *___qtablewidgetitem11 = tableWidget->item(5, 0);
+        ___qtablewidgetitem11->setText(QApplication::translate("MainWindow", "40", Q_NULLPTR));
         tableWidget->setSortingEnabled(__sortingEnabled);
 
     } // retranslateUi
