3f461746375d6dee133fcc78f1aaf01e03cd5b9e
Ваше Имя <you@example.com>
2020-09-17 17:33:34 +0300

    1

--- linux_vars/mainwindow.cpp
+++ linux_vars/mainwindow.cpp
@@ -1,11 +1,29 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QTextStream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+        QFile file("/proc/cpuinfo");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return;
+
+    QTextStream in(&file);
+    //while (!in.atEnd()) {
+        QString line = in.readLine();
+        QStringList list = line.split(":");
+    //    process_line(line);
+    //}
+
+
+    ui->tableWidget->setItem(0,0,new QTableWidgetItem(list[0]));
+    ui->tableWidget->setItem(0,1,new QTableWidgetItem("010101"));
+
 }
 
 MainWindow::~MainWindow()
