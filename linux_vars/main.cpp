b4ddf22c78f623c2ad998806156310e0c5b0e1ca
Ваше Имя <you@example.com>
2020-09-18 11:22:12 +0300

    1

--- linux_vars/main.cpp
+++ linux_vars/main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "QThread"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    //QThread::sleep(3);
+    w.get_info();
 
     return a.exec();
 }
--- linux_vars/mainwindow.cpp
+++ linux_vars/mainwindow.cpp
@@ -8,7 +8,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    get_proc_info();
+    //get_info();
 }
 
 MainWindow::~MainWindow()
@@ -16,20 +16,46 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::get_proc_info()
+void MainWindow::get_info()
 {
-        QFile file("/proc/cpuinfo");
+    QString f_cpu_info = "/proc/cpuinfo";
+    QString s_cpu_model = "model name";
+    QString v_cpu_model;
+    QString s_cpu_freq = "cpu MHz";
+    QString s_cpu_cs = "cache size";
+
+    QString f_cpu_load = "/proc/stat";
+    QString s_cpu_load = "cpu ";
+
+    QString f_mem = "/proc/meminfo";
+    QString s_mem_total = "MemTotal";
+    QString s_mem_free = "MemFree";
+
+    QString f_bat = "/sys/class/power_supply/BAT0/capacity";
+
+    QFile file(f_cpu_info);
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
         return;
 
+// Get proc info
+    QString var;
     QTextStream in(&file);
-    //while (!in.atEnd()) {
+
+    std::string s1, s2, s3;
+    while (!in.atEnd()) {
         QString line = in.readLine();
-        QStringList list = line.split(":");
-    //    process_line(line);
+
+        //if(line.contains(s_cpu_model))
+    //{
+            QStringList list = line.split(":");
+            var = list[0];
+            s1 = var.toStdString();
+            v_cpu_model = list[1];
     //}
+    //    process_line(line);
+    }
 
 
-    ui->tableWidget->setItem(0,0,new QTableWidgetItem(list[0]));
-    ui->tableWidget->setItem(0,1,new QTableWidgetItem("010101"));
+    ui->tableWidget->setItem(0,0,new QTableWidgetItem(v_cpu_model));
+    ui->tableWidget->setItem(0,1,new QTableWidgetItem(var));
 }
