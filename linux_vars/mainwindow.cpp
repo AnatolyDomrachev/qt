6848b904d480033ad3c5798a4529444534786e86
Ваше Имя <you@example.com>
2020-09-18 13:35:47 +0300

    1

--- linux_vars/mainwindow.cpp
+++ linux_vars/mainwindow.cpp
@@ -18,45 +18,105 @@ MainWindow::~MainWindow()
 
 void MainWindow::get_info()
 {
-    QString f_cpu_info = "/proc/cpuinfo";
-    QString s_cpu_model = "model name";
-    QString v_cpu_model;
-    QString s_cpu_freq = "cpu MHz";
-    QString s_cpu_cs = "cache size";
+    get_cpu_info();
+    get_cpu_load();
+    get_mem_load();
+    get_bat_load();
+}
 
-    QString f_cpu_load = "/proc/stat";
-    QString s_cpu_load = "cpu ";
-
-    QString f_mem = "/proc/meminfo";
-    QString s_mem_total = "MemTotal";
-    QString s_mem_free = "MemFree";
-
-    QString f_bat = "/sys/class/power_supply/BAT0/capacity";
+void MainWindow::get_cpu_info()
+{
 
     QFile file(f_cpu_info);
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
         return;
 
 // Get proc info
-    QString var;
     QTextStream in(&file);
+        QString line;
+            QStringList list ;
+    int num_vars = 0;
 
-    std::string s1, s2, s3;
-    while (true) {
-        QString line = in.readLine();
+    for(int i=0; i<1000; i++){
+        line = in.readLine();
 
       if(line.contains(s_cpu_model))
       {
-            QStringList list = line.split(":");
-            var = list[0];
-            s1 = var.toStdString();
+            list = line.split(":");
             v_cpu_model = list[1];
-            break;
+	    num_vars++;
       }
+
+      if(line.contains(s_cpu_cs))
+      {
+            list = line.split(":");
+            v_cpu_cs = list[1];
+	    num_vars++;
+      }
+
+      if(line.contains(s_cpu_freq))
+      {
+            list = line.split(":");
+            v_cpu_freq = list[1];
+	    num_vars++;
+      }
+
+      if(num_vars==3)
+	break;
 
     }
 
-
     ui->tableWidget->setItem(0,0,new QTableWidgetItem(v_cpu_model));
-    ui->tableWidget->setItem(0,1,new QTableWidgetItem(var));
+    ui->tableWidget->setItem(0,1,new QTableWidgetItem(v_cpu_freq));
+    ui->tableWidget->setItem(0,2,new QTableWidgetItem(v_cpu_cs));
 }
+void MainWindow::get_cpu_load()
+{
+    double Idle , NonIdle ;
+    QString idle , iowait , user , nice , system , irq , softirq , steal;
+
+    QFile file(f_cpu_load);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return;
+
+    QTextStream in(&file);
+    QString line;
+    QStringList list ;
+    QRegularExpression regex( " +" );
+
+    for(int i=0; i<1000; i++){
+        line = in.readLine();
+
+      if(line.contains(s_cpu_load))
+      {
+            QStringList list = line.split(regex);
+            idle = list[4];
+            iowait = list[5];
+
+user  = list[1];
+nice  = list[2];
+system  = list[3];
+irq  = list[6];
+softirq  = list[7];
+steal = list[8];
+
+            Idle = idle.toInt() + iowait.toInt();
+            NonIdle = user.toInt() + nice.toInt() + system.toInt() + irq.toInt() + softirq.toInt() + steal.toInt();
+            v_cpu_load = NonIdle/(Idle + NonIdle)*100;
+            break;
+      }
+    }
+
+    QString result = QString::number(v_cpu_load, 'g', 3);
+
+    ui->tableWidget->setItem(0,3,new QTableWidgetItem(result));
+}
+
+void MainWindow::get_mem_load()
+{
+}
+
+void MainWindow::get_bat_load()
+{
+}
+
--- linux_vars/mainwindow.h
+++ linux_vars/mainwindow.h
@@ -18,6 +18,29 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    QString f_cpu_info = "/proc/cpuinfo";
+    QString s_cpu_model = "model name";
+    QString v_cpu_model;
+    QString s_cpu_freq = "cpu MHz";
+    QString v_cpu_freq;
+    QString s_cpu_cs = "cache size";
+    QString v_cpu_cs;
+
+    QString f_cpu_load = "/proc/stat";
+    QString s_cpu_load = "cpu ";
+    double v_cpu_load;
+
+    QString f_mem = "/proc/meminfo";
+    QString s_mem_total = "MemTotal";
+    QString s_mem_free = "MemFree";
+
+    QString f_bat = "/sys/class/power_supply/BAT0/capacity";
+
+    void get_cpu_info();
+    void get_cpu_load();
+    void get_mem_load();
+    void get_bat_load();
 };
 
 #endif // MAINWINDOW_H
